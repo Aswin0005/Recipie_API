0443371d429191e6b4bcee82cab7136ccd71263b
Aswin <aswinash19@gmail.com>
2024-05-03 02:53:10 +0530

    Testing

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ const searchRouter = require('./routes/search')
 const jobsRouter = require('./routes/jobs');
 const allRouter = require('./routes/alljob')
 app.use(express.json())
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://recipie-api-3tr8.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 
 app.get('/',(req,res) => {
     res.send('Hello')
